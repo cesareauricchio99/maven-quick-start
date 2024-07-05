3cf46a2eacd8a70437a8b70485152145c2edc05e
Cesare Auricchio <Cesare.Auricchio@ibm.com>
2024-07-05 10:35:06 +0200

    Pushing changes with Java App and Pom XML with dependencies

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -2,6 +2,7 @@ package clinic.programming.training;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
     public void greet(){
@@ -11,6 +12,13 @@ public class Application {
 		System.out.println("Greeting:" + greeting);
 		}
 	}
+
+	public int countWords(String words) {
+	String [] separateWords = StringUtils.split(words, ' ');
+	return (separateWords == null) ? 0 : separateWords.length;
+	}
+		
+	
     public Application() {
         System.out.println ("Inside Application");
     }
@@ -20,5 +28,7 @@ public class Application {
     	System.out.println ("Starting Application");
 	Application app = new Application();
 	app.greet();
+	int count = app.countWords("I have four words");
+	System.out.println("Word count" + count);
     }
 }
